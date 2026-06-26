3887eb33cdcf1a0d9ace045ab00779ce0b31e7d0
kiranasekayu-byte <kiranasekayu@gmail.com>
2026-06-26 21:22:48 +0700

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 genai.configure(api_key=os.getenv("API_KEY"))
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 @app.route("/")
 def home():
